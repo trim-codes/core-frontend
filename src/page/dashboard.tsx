0a7e793378a96cac5b0aba8886458a20628244aa
Dumbiri Chuks <dumbirichuks@gmail.com>
2025-03-29 01:56:26 +0100

    updated

--- src/page/dashboard.tsx
+++ src/page/dashboard.tsx
@@ -1,92 +1,20 @@
-// import React from 'react'
+import React from 'react'
 
-// const DashboardScreen = () => {
-//     return (
-//         <div>DashbaordScreen</div>
-//     )
-// }
-
-// export default DashboardScreen
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-
-import {
-    Sidebar,
-    SidebarContent,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from "../components/ui/sidebar"
-
-const items = [
-    {
-        title: "Home",
-        url: "#",
-        icon: Home,
-    },
-    {
-        title: "Inbox",
-        url: "#",
-        icon: Inbox,
-    },
-    {
-        title: "Calendar",
-        url: "#",
-        icon: Calendar,
-    },
-    {
-        title: "Search",
-        url: "#",
-        icon: Search,
-    },
-    {
-        title: "Settings",
-        url: "#",
-        icon: Settings,
-    },
-]
-
-export const DashboardScreen = () => {
+const DashboardScreen = () => {
     return (
-        <Sidebar>
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel>Application</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <a href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </a>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-        </Sidebar>
+        <div>Dashboard</div>
     )
 }
 
 export default DashboardScreen
+
+
+
+
+
+
+
+
+
+
+
